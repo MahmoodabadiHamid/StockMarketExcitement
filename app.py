54ee69a421af4180de4f43c290ee40e0326b3391
Hamid Mahmoodabadi <mahmoodabadihamid@gmail.com>
2024-04-21 08:43:36 +0330

    multipage reformed

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ if True:
    
 
     app.layout = html.Div(
-    [        
-        dash.page_container,
-    ]
+        [        
+            dash.page_container,
+        ]
     )
-    
 
     if __name__ == "__main__":
         app.run_server(debug=True, port='3559')
